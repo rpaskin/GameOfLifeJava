0fdc63bbc19ca992575e12f517c38e6f94a2c980
rpaskin <ronnie@megaputz.com>
2015-08-04 16:30:07 -0300

    Adiciona 8 quadrados

--- GameOfLife.java
+++ GameOfLife.java
@@ -20,6 +20,11 @@ class GameOfLife implements ActionListener
 		Square square1 = new Square();
 		Square square2 = new Square();					
 		Square square3 = new Square();
+		Square square4 = new Square();
+		Square square5 = new Square();
+		Square square6 = new Square();
+		Square square7 = new Square();
+		Square square8 = new Square();
 
 		square1.setXY(0, 0);
 		square1.setAlive(true);	
@@ -27,12 +32,32 @@ class GameOfLife implements ActionListener
 		square2.setXY(1, 0);
 		square2.setAlive(true);	
 
-		square3.setXY(2, 0);
+		square3.setXY(0, 1);
 		square3.setAlive(true);	
+
+		square4.setXY(1, 1);
+		square4.setAlive(true);
+
+		square5.setXY(2, 2);
+		square5.setAlive(true);	
+
+		square6.setXY(2, 3);
+		square6.setAlive(true);	
+
+		square7.setXY(3, 2);
+		square7.setAlive(true);	
+
+		square8.setXY(3, 3);
+		square8.setAlive(true);	
 
 		board.addSquare(square1);
 		board.addSquare(square2);							
-		board.addSquare(square3);							
+		board.addSquare(square3);	
+		board.addSquare(square4);
+		board.addSquare(square5);							
+		board.addSquare(square6);
+		board.addSquare(square7);
+		board.addSquare(square8);							
 
 
 		MyDrawPanel drawPanel = new MyDrawPanel();
